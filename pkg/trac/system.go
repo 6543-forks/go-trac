d9d6df7f4ce68428ccbc4c0e87a1312c3cfd8cac
ics <iscandr@gmail.com>
2017-09-14 22:33:55 +0200

    Add doc to system functions

--- pkg/trac/system.go
+++ pkg/trac/system.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 )
 
+// APIVersion represents the remote version.
+// First element is the epoch (0=Trac 0.10, 1=Trac 0.11 or higher). Second
+// element is the major version number, third is the minor.
 type APIVersion struct {
 	Epoch int
 	Major int
 	Minor int
 }
 
+// UnmarshalJSON unmarshals returned array into an APIVersion
 func (v *APIVersion) UnmarshalJSON(in []byte) error {
 	ver := []interface{}{
 		&v.Epoch,
@@ -23,6 +27,7 @@ func (v *APIVersion) UnmarshalJSON(in []byte) error {
 	return nil
 }
 
+// System represents the core of the RPC system.
 type System struct {
 	client *Client
 }
